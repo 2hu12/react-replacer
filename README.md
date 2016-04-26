616e015368cdbb80380faec4a228f1701db64067
zhulongzheng <zhulongzheng@gmail.com>
2016-04-26 22:29:35 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # react-replacer
 Replace strings within a Reactjs component, to avoid using `dangerouslySetInnerHTML`.
+
+## Usage
+
+### Single Config
+
+`replacer(replacement)(rawString | pattern)(pattern | rawString)`, the params order aims for `curry` and the last two params must be either `rawString and pattern` or `pattern and rawString`, if you chose pattern as the last param, pattern's type can be both `RegExp` or `String`, otherwise it must be type of `RegExp`. The type of `replacement` can be both `Function` or `String`, and `replacement` function will be feeded with `...[key, ...pattern.exec(match)]`.
+
+**Arguments**
+
+1. replacement (Function | String)
+2. rawString (String) | pattern (RegExp)
+3. pattern (RegExp | String) | rawString (String)
+
+**Returns**
+
+Returns a React children array.
+
+**Example**
+
+```jsx
+import { replacer } from 'react-replacer'
+
+let highlighter = replacer(function(keyid, match) {
+    return <span key={keyid} className="highlight">{match}</span>
+})('This should be hightlighted!')
+
+<h3 className="sidebar-friendlist__name">
+    {highlighter(/highlight/)}
+</h3>
+
+=>
+
+<h3><span>This should be </span><span class="highlight">highlight</span><span>ed!</span></h3>
+```
+
+### Group Config
+
+`greplacer(rules)(rawString)`
+
+**Arguments**
+
+1. rules (Array): The array of rule object, which contains two property `pattern` and `replacement`, they are just like the single config param.
+2. rawString (String): The string to be processed.
+
+**Returns**
+
+(Array): Returns a React children array.
+
+**Example**
+
+```jsx
+import { greplacer } from 'react-replacer'
+
+let highlighter = replacer()('This should be hightlighted!')
+
+<h3 className="sidebar-friendlist__name">
+    {highlighter(/highlight/)}
+</h3>
+
+
+let decoder = greplacer([{
+  pattern: /hightlight/g,
+  replacement: function(keyid, match) {
+    return <span key={keyid} className="highlight">{match}</span>
+  }
+}, {
+  pattern: 'original',
+  replacement: function(keyid, match) {
+    return <span key={keyid} className="replaced">{match + ' replaced'}</span>
+  }
+}])
+
+<h3>{decoder('left highlight and original right')}</h3>
+
+=>
+
+<h3><span>left </span><span class="highlight">highlight</span><span> and </span><span class="replaced">original replaced</span><span>ed!</span></h3>
+```
---
+++ src/index.js
@@ -0,0 +1,103 @@
+import React from 'react'
+
+let keyid = 0
+
+function plain(tag, keyid, str) {
+  return React.DOM[tag]({ key: keyid }, str);
+}
+
+function span(keyid, str) {
+  return plain('span', keyid, str)
+}
+
+function flattenFrom(arr) {
+  if (arr.length === 1 && arr[0] instanceof Array) {
+    return arr[0]
+  }
+  return arr
+}
+
+export default function replaceChildren(rules) {
+  if (!(rules instanceof Array)) rules = [rules]
+
+  return function (raw) {
+    if (!rules.length || !raw.length) return [span(keyid++, raw)]
+
+    let [head, ...tail] = rules
+
+    if (typeof head.pattern === 'string' && !head.pattern.length) {
+      return replaceChildren(tail)(raw)
+    }
+
+    // checkout String.prototype.split(), Capturing parentheses
+    if (head.pattern instanceof RegExp) {
+      let source = head.pattern.source
+
+      if (!/^\(.*\)$/.test(source)) {
+        head.pattern = new RegExp(`(${source})`, head.pattern.global
+          ? head.pattern.flags
+          : head.pattern.flags + 'g')
+      }
+    } else {
+      if (!/^\(.*\)$/.test(head.pattern)) {
+        head.pattern = new RegExp(`(${head.pattern})`, 'g')
+      }
+    }
+
+    let children = []
+    let lastIndex = 0
+    let match
+
+    while(match = head.pattern.exec(raw)) {
+      if (match.index > lastIndex) {
+        children.push(replaceChildren(tail)(raw.slice(lastIndex, match.index)))
+      }
+      children.push(head.replacement(keyid++, match[0], ...match.slice(2)))
+      lastIndex = match.index + match[0].length
+    }
+
+    if (lastIndex < raw.length) {
+      children.push(replaceChildren(tail)(raw.slice(lastIndex)))
+    }
+
+    return children
+  }
+}
+
+export const replacer = (replacement) => (raw) => {
+  return (pattern) => {
+    if (raw instanceof RegExp) {
+      [pattern, raw] = [raw, pattern]
+    }
+
+    replaceChildren({
+      pattern,
+      replacement
+    })(raw)
+  }
+}
+
+
+export function greplacer(...rules) {
+  rules = flattenFrom(rules)
+
+  return function(raw) {
+    return replaceChildren(rules)(raw)
+  }
+}
+
+export function extendableGreplacers(...rules) {
+  rules = flattenFrom(rules)
+
+  function extendable(raw) {
+    return replaceChildren(rules)(raw)
+  }
+
+  extendable.extend = function(...newConf) {
+    return function(raw) {
+      return replaceChildren(rules.concat(newConf))(raw)
+    }
+  }
+
+  return extendable
+}
